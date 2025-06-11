2f09a4f15b5a6b12a4376fba91048e0881afa62f
Yohan DEBUSSCHER <yohan.debusscher@isalyx.group>
2025-06-12 00:24:00 +0200

    Modification du prefab boat, player. Ajout des scripts inputs, servicelocator

---
+++ Assets/Scripts/Controller/UIController.cs
@@ -0,0 +1,178 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+public class UIController : MonoBehaviour
+{
+    [Header("Navigation Settings")]
+    [SerializeField] private GameObject firstSelected;
+    [SerializeField] private bool lockCursorOnDisable = true;
+
+    [Header("Menu Management")]
+    [SerializeField] private List<GameObject> menuPanels = new List<GameObject>();
+    [SerializeField] private int currentPanelIndex = 0;
+
+    private Stack<GameObject> menuHistory = new Stack<GameObject>();
+    private bool wasUsingGamepad = false;
+
+    private void OnEnable()
+    {
+        SetupUI();
+
+        InputService.OnUINavigate += HandleNavigate;
+        InputService.OnUISubmit += HandleSubmit;
+        InputService.OnUICancel += HandleCancel;
+    }
+
+    private void OnDisable()
+    {
+        InputService.OnUINavigate -= HandleNavigate;
+        InputService.OnUISubmit -= HandleSubmit;
+        InputService.OnUICancel -= HandleCancel;
+
+        if (lockCursorOnDisable)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    private void Update()
+    {
+        HandleInputDeviceSwitch();
+    }
+
+    private void SetupUI()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        if (EventSystem.current != null && firstSelected != null)
+        {
+            EventSystem.current.SetSelectedGameObject(firstSelected);
+        }
+
+        Time.timeScale = 0f;
+    }
+
+    private void HandleNavigate(Vector2 direction)
+    {
+
+        if (direction.magnitude > 0.1f)
+        {
+            wasUsingGamepad = true;
+
+            if (EventSystem.current.currentSelectedGameObject == null && firstSelected != null)
+            {
+                EventSystem.current.SetSelectedGameObject(firstSelected);
+            }
+        }
+    }
+
+    private void HandleSubmit()
+    {
+        if (EventSystem.current?.currentSelectedGameObject != null)
+        {
+            ExecuteEvents.Execute(
+                EventSystem.current.currentSelectedGameObject,
+                new BaseEventData(EventSystem.current),
+                ExecuteEvents.submitHandler
+            );
+        }
+    }
+
+    private void HandleCancel()
+    {
+        if (menuHistory.Count > 0)
+        {
+            GoToPreviousMenu();
+        }
+        else
+        {
+            CloseUI();
+        }
+    }
+
+    private void HandleInputDeviceSwitch()
+    {
+        bool usingMouse = Input.mousePosition != Input.mousePosition;
+
+        if (wasUsingGamepad && usingMouse)
+        {
+            wasUsingGamepad = false;
+            EventSystem.current.SetSelectedGameObject(null);
+        }
+    }
+
+    public void OpenMenu(GameObject menuPanel)
+    {
+        if (menuPanel == null) return;
+
+        var currentSelected = EventSystem.current?.currentSelectedGameObject;
+        if (currentSelected != null)
+        {
+            menuHistory.Push(currentSelected);
+        }
+
+        foreach (var panel in menuPanels)
+        {
+            panel.SetActive(false);
+        }
+
+        menuPanel.SetActive(true);
+
+        var selectables = menuPanel.GetComponentsInChildren<UnityEngine.UI.Selectable>();
+        if (selectables.Length > 0)
+        {
+            EventSystem.current.SetSelectedGameObject(selectables[0].gameObject);
+        }
+    }
+
+    public void GoToPreviousMenu()
+    {
+        if (menuHistory.Count > 0)
+        {
+            var previousMenu = menuHistory.Pop();
+            if (previousMenu != null)
+            {
+                EventSystem.current.SetSelectedGameObject(previousMenu);
+            }
+        }
+    }
+
+    public void CloseUI()
+    {
+        Time.timeScale = 1f;
+
+        var inputService = GameServiceLocator.Get<InputService>();
+        inputService?.SwitchToActionMap("Character"); 
+
+        gameObject.SetActive(false);
+
+        Debug.Log("[UIController] UI closed");
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void OnResumeClicked()
+    {
+        CloseUI();
+    }
+
+    public void OnOptionsClicked()
+    {
+        Debug.Log("[UIController] Options menu requested");
+    }
+
+    public void OnQuitClicked()
+    {
+        QuitGame();
+    }
+}
---
+++ Assets/Scripts/Services/InputService.cs
@@ -0,0 +1,151 @@
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class InputService : MonoBehaviour
+{
+    [Header("Input Settings")]
+    [SerializeField] private string defaultActionMap = "Character";
+
+    private PlayerControls playerControls;
+    private string currentActionMap;
+
+    public event System.Action<string> OnActionMapChanged;
+    public event System.Action<string, string> OnActionMapSwitched;
+
+    public static event System.Action<Vector2> OnCharacterMove;
+    public static event System.Action<Vector2> OnCharacterLook;
+    public static event System.Action OnCharacterJump;
+    public static event System.Action OnCharacterInteract;
+    public static event System.Action<bool> OnCharacterSprint;
+
+    public static event System.Action<float> OnBoatSteer;
+    public static event System.Action OnBoatCancel;
+    public static event System.Action OnBoatToggleSails;
+
+    public static event System.Action<Vector2> OnUINavigate;
+    public static event System.Action OnUISubmit;
+    public static event System.Action OnUICancel;
+
+    public string CurrentActionMap => currentActionMap;
+    public PlayerControls Controls => playerControls;
+
+    private void Awake()
+    {
+        GameServiceLocator.Register<InputService>(this,
+            GameServiceLocator.ServiceLifecycle.Persistent,
+            false,
+            "Main Input Manager");
+
+        playerControls = new PlayerControls();
+
+        SetupInputCallbacks();
+
+        SwitchToActionMap(defaultActionMap);
+    }
+
+    private void SetupInputCallbacks()
+    {
+        // Character callbacks
+        playerControls.Character.Move.performed += ctx => OnCharacterMove?.Invoke(ctx.ReadValue<Vector2>());
+        playerControls.Character.Move.canceled += _ => OnCharacterMove?.Invoke(Vector2.zero);
+
+        playerControls.Character.Look.performed += ctx => OnCharacterLook?.Invoke(ctx.ReadValue<Vector2>());
+        playerControls.Character.Look.canceled += _ => OnCharacterLook?.Invoke(Vector2.zero);
+
+        playerControls.Character.Jump.performed += _ => OnCharacterJump?.Invoke();
+        playerControls.Character.Interact.performed += _ => OnCharacterInteract?.Invoke();
+
+        playerControls.Character.Sprint.performed += _ => OnCharacterSprint?.Invoke(true);
+        playerControls.Character.Sprint.canceled += _ => OnCharacterSprint?.Invoke(false);
+
+        // Boat callbacks
+        playerControls.Boat.Steer.performed += ctx => OnBoatSteer?.Invoke(ctx.ReadValue<float>());
+        playerControls.Boat.Steer.canceled += _ => OnBoatSteer?.Invoke(0f);
+        playerControls.Boat.Cancel.performed += _ => OnBoatCancel?.Invoke();
+        playerControls.Boat.ToggleSails.performed += _ => OnBoatToggleSails?.Invoke();
+
+        // UI callbacks
+        playerControls.UI.Navigate.performed += ctx => OnUINavigate?.Invoke(ctx.ReadValue<Vector2>());
+        playerControls.UI.Submit.performed += _ => OnUISubmit?.Invoke();
+        playerControls.UI.Cancel.performed += _ => OnUICancel?.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        playerControls?.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls?.Disable();
+    }
+
+    private void OnDestroy()
+    {
+        ClearAllCallbacks();
+
+        playerControls?.Dispose();
+        GameServiceLocator.Unregister<InputService>();
+    }
+    private void ClearAllCallbacks()
+    {
+        OnCharacterMove = null;
+        OnCharacterLook = null;
+        OnCharacterJump = null;
+        OnCharacterInteract = null;
+        OnCharacterSprint = null;
+
+        OnBoatSteer = null;
+        OnBoatCancel = null;
+        OnBoatToggleSails = null;
+
+        OnUINavigate = null;
+        OnUISubmit = null;
+        OnUICancel = null;
+    }
+
+    public void SwitchToActionMap(string mapName)
+    {
+        if (playerControls == null) return;
+
+        string previousMap = currentActionMap;
+
+        if (!string.IsNullOrEmpty(currentActionMap))
+        {
+            var currentMap = GetActionMap(currentActionMap);
+            currentMap?.Disable();
+        }
+
+        var newMap = GetActionMap(mapName);
+        if (newMap != null)
+        {
+            newMap.Enable();
+            currentActionMap = mapName;
+
+            OnActionMapChanged?.Invoke(mapName);
+            OnActionMapSwitched?.Invoke(previousMap, mapName);
+
+            Debug.Log($"[InputService] Switched from '{previousMap}' to '{mapName}'");
+        }
+        else
+        {
+            Debug.LogWarning($"[InputService] Action Map '{mapName}' not found!");
+        }
+    }
+
+    private InputActionMap GetActionMap(string mapName)
+    {
+        return mapName.ToLower() switch
+        {
+            "character" => playerControls.Character,
+            "ui" => playerControls.UI,
+            "boat" => playerControls.Boat,
+            _ => null
+        };
+    }
+
+    public bool IsActionMapActive(string mapName)
+    {
+        return currentActionMap?.Equals(mapName, System.StringComparison.OrdinalIgnoreCase) ?? false;
+    }
+}
