420303e36640d24705bfd25b5f4e584f8becb7ba
emil <ehp84@sfr.fr>
2025-06-11 12:06:15 +0200

    ajout load & shoot ranged canon

--- Assets/Scripts/Gameplay/Canon/CanonLoader.cs
+++ Assets/Scripts/Gameplay/Canon/CanonLoader.cs
@@ -5,10 +5,20 @@ public class CanonLoader : MonoBehaviour, ILoadable
     [field: SerializeField]
     public bool alreadyLoad { get; set; }
 
+    [SerializeField]
+    private int m_maxBallstock;
+
+    [field: SerializeField]
+    public int currentBallStock { get; set; }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (alreadyLoad == false && other.GetComponent<ILoadable>() != null)
+        if (currentBallStock < m_maxBallstock && other.GetComponent<ILoadable>() != null)
         {
+            if(currentBallStock < m_maxBallstock)
+            {
+                currentBallStock++;
+            }
             other.GetComponent<ILoadable>().Load(true);
             Load(true);
         }
---
+++ Assets/Scripts/Gameplay/Canon/RangedCanonController.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedCanonController : MonoBehaviour, IShootable, IInteractable
+{
+    [SerializeField]
+    private CanonLoader m_canonLoader;
+
+    [SerializeField]
+    private GameObject m_projectile;
+
+    [SerializeField]
+    private List<GameObject> m_muzzle = new List<GameObject>();
+
+    void Start()
+    {
+        m_canonLoader = GetComponent<CanonLoader>();
+    }
+
+    void Update()
+    {
+       
+    }
+
+    public void Interact(IPlayer player)
+    {
+        
+    }
+
+    public void Shoot()
+    {
+        if (m_canonLoader == null) return;
+        if (m_canonLoader.alreadyLoad == true)
+        {
+            for(int i = 0; i < m_canonLoader.currentBallStock; i++)
+            {
+                Instantiate(m_projectile, m_muzzle[i].transform.position, Quaternion.identity, null);
+            }
+            m_canonLoader.GetComponent<ILoadable>().Load(false);
+
+        }
+    }
+}
